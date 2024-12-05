78cd688d8fc2addf41d6f8dee89e0d7c1d448725
Cool <990299103@qq.com>
2024-12-05 17:18:57 +0800

    feat："接口数据Mock"

---
+++ example-consumer/src/main/java/com/cool/example/consumer/ConsumerExample03.java
@@ -0,0 +1,27 @@
+package com.cool.example.consumer;
+
+import com.cool.example.model.User;
+import com.cool.example.service.UserService;
+import com.cool.meta.proxy.ServiceProxyFactory;
+
+/**
+ * 简易服务消费者示例
+ */
+public class ConsumerExample03 {
+
+    public static void main(String[] args) {
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("cool");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+        long number = userService.getNumber();
+        System.out.println(number);
+    }
+}
